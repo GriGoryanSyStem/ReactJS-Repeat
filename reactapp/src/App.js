6cd243bf4e49d20ed3259a859ba896a42f1f1e7b
GriGoryanSyStem <narekgr90@gmail.com>
2020-04-03 23:07:19 +0400

    11. Grid

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,16 +1,43 @@
 import React from 'react';
 import './App.css';
-import Header from "../components/Header";
-import Technologies from "../components/Technologies";
-import Footer from "../components/Footer";
+import pic from "../src/pictures/logo.svg"
 
 const App = () => {
-  return (
-    <div className="App">
-      <Header/>
-      <Technologies/>
-      <Footer/>
-    </div>
-  );
+    return (
+        <div className="appWrapper">
+            <header className="header">
+                <img src={pic} alt="pic" className="pic"/>
+
+            </header>
+            <nav className="nav">
+                <div>
+                    <a>Profile</a>
+                </div>
+                <div>
+                    <a>Massages</a>
+                </div>
+                <div>
+                    <a>News</a>
+                </div>
+                <div>
+                    <a>Users</a>
+                </div>
+                <div>
+                    <a>Settings</a>
+                </div>
+            </nav>
+            <div className="content">
+                <div>
+                    Main Content
+                </div>
+                <div>
+                    Users
+                </div>
+                <div>
+                    Friends
+                </div>
+            </div>
+        </div>
+    );
 };
 export default App;
