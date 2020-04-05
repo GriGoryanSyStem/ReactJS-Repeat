c4c3298ae121ce7cb9a5e875a447d8d413774b6e
GriGoryanSyStem <narekgr90@gmail.com>
2020-04-06 00:37:01 +0400

    27. Route component vs render, props через Route
    Что делает рендер компонента Route?
    У Route есть 3 props'a которые описывают каким образом выполнить рендер сопоставляя prop path с location.pathname и только один из prop должен быть представлен в Route:
    
    1. component — React компонент. Когда роут удовлетворяется сопоставление в path, то он возвращает переданный component (используя функцию React.createElement).
    
    2. render — функция которая должна вернуть элемент React. Будет вызвана когда удовлетворится сопоставление в path. Render довольно похож на component, но используется для inline рендеринга и подстановки необходимых для элемента props[5].
    
    3. children — в отличие от предыдущих двух props children будет всегда отображаться независимо от того сопоставляется ли path или нет.

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -12,10 +12,16 @@ const App = () => {
         <div className="appWrapper">
             <Header/>
             <NavBar/>
-            <Route component={Profile} path="/profile"/>
-            <Route component={Dialogs} path="/dialogs"/>
+            <Route render={()=><Profile/>} path="/profile"/>
+            <Route render={()=><Dialogs/>} path="/dialogs"/>
         </div>
         </BrowserRouter>
     );
 };
 export default App;
+
+//Что делает рендер компонента Route?
+// У Route есть 3 props'a которые описывают каким образом выполнить рендер сопоставляя prop path с location.pathname и только один из prop должен быть представлен в Route://
+// component — React компонент. Когда роут удовлетворяется сопоставление в path, то он возвращает переданный component (используя функцию React.createElement).
+// render — функция которая должна вернуть элемент React. Будет вызвана когда удовлетворится сопоставление в path. Render довольно похож на component, но используется для inline рендеринга и подстановки необходимых для элемента props[5].
+// children — в отличие от предыдущих двух props children будет всегда отображаться независимо от того сопоставляется ли path или нет.
