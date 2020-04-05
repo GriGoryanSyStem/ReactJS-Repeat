e597e2d1d5e320a89c000c5ae1dfc994949a52dc
GriGoryanSyStem <narekgr90@gmail.com>
2020-04-05 14:38:28 +0400

    22. Css two Classes, NavLink activeClassName

--- reactapp/src/components/Dialogs/Dialogs.jsx
+++ reactapp/src/components/Dialogs/Dialogs.jsx
@@ -1,20 +1,32 @@
 import React from "react";
 import c from "./Dialogs.module.css"
+import {NavLink} from "react-router-dom";
 
-const Dialogs = () =>{
+const Dialogs = () => {
     return (
         <div className={c.dialogs}>
+
             <div className={c.dialogsItem}>
-                <div className={`${c.dialog} ${c.active}`}>Dina</div>
-                <div className={c.dialog}>Valeria</div>
-                <div className={c.dialog}>Sasha</div>
-                <div className={c.dialog}>Andrey</div>
+                <div className={c.dialog}>
+                    <NavLink to="/dialogs/1" activeClassName={c.selected}>Dina</NavLink>
+                </div>
+                <div className={c.dialog}>
+                    <NavLink to="/dialogs/2" activeClassName={c.selected}>Andrey</NavLink>
+                </div>
+                <div className={c.dialog}>
+                    <NavLink to="/dialogs/3" activeClassName={c.selected}>Maria</NavLink>
+                </div>
+                <div className={c.dialog}>
+                    <NavLink to="/dialogs/4" activeClassName={c.selected}>Pol</NavLink>
+                </div>
             </div>
-           <div className={c.massagesItem}>
-               <div className={c.massage}>Hi</div>
-               <div className={c.massage}>How are you</div>
-               <div className={c.massage}>React</div>
-           </div>
+
+            <div className={c.massagesItem}>
+                <div className={c.massage}>Hi</div>
+                <div className={c.massage}>How are you</div>
+                <div className={c.massage}>React</div>
+            </div>
+
         </div>
     )
 };
