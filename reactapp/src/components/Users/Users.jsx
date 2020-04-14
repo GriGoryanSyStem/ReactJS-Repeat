9268693186242ef159744b60daf59a6eb4159d8f
GriGoryanSyStem <narekgr90@gmail.com>
2020-04-14 22:52:48 +0400

    52. Side Effect, notihing
    Side effect example
    1. api calls
    2.modification of parameters
    3.change DOM directly
    4.change outer scope variables
    5....
    
    maqur func chpetq e ani koxmnaki ban baci jsx veradardzneluc

--- reactapp/src/components/Users/Users.jsx
+++ reactapp/src/components/Users/Users.jsx
@@ -4,28 +4,30 @@ import axios from "axios";
 import img from "../../pictures/personUser.png";
 
 const Users = (props) => {
-    axios.get('https://social-network.samuraijs.com/api/1.0/users')
-        .then(response => {
-            return props.setUsers(response.data.items);
-        });
-
+    let getApiUsers = () => {
+        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+            .then(response => {
+                return props.setUsers(response.data.items);
+            });
+    };
     return (
         <div className={c.usersContainer}>
+            <button onClick={getApiUsers}> Get Api Users</button>
             {props.usersReducer.usersData.map((i, k) => {
                 return (
                     <div key={k} className={c.userItem}>
                         <div className={c.userItemImage}>
                             <div className={c.imageButton}>
                                 <img src={i.photos.small ? i.photos.small : img} alt="photo"/>
-                               <div>
-                                <button className={c.button}
-                                        onClick={(e) => {
-                                            e.currentTarget.innerText === 'Follow'
-                                                ? props.followFunc(false, i.id)
-                                                : props.followFunc(true, i.id)
-                                        }}>{i.followed ? 'Follow' : "UnFollow"}
-                                </button>
-                               </div>
+                                <div>
+                                    <button className={c.button}
+                                            onClick={(e) => {
+                                                e.currentTarget.innerText === 'Follow'
+                                                    ? props.followFunc(false, i.id)
+                                                    : props.followFunc(true, i.id)
+                                            }}>{i.followed ? 'Follow' : "UnFollow"}
+                                    </button>
+                                </div>
                             </div>
                         </div>
                         <div>User ID : {i.id}</div>
