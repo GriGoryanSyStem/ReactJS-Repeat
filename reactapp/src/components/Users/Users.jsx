2dc4bf095dc6060ce39a3953dcb8df98b65d53f8
GriGoryanSyStem <narekgr90@gmail.com>
2020-04-14 23:24:02 +0400

    53. Class Component, extends React.Component
    constructore kanchvum e mek angam u heto render e linum, dra hamar axios enq anum construcrori mej vor side effect chlini, u vstah linenq vor get exela nor rendera linum,
    !!!!! bayc ajax zaprosnery sovorabar senc u stex chen anum, ayl heto api.js ksarqenq sax klcnenq yndex thunkerov

--- reactapp/src/components/Users/Users.jsx
+++ reactapp/src/components/Users/Users.jsx
@@ -3,40 +3,45 @@ import c from "./Users.module.css";
 import axios from "axios";
 import img from "../../pictures/personUser.png";
 
-const Users = (props) => {
-    let getApiUsers = () => {
+class Users extends React.Component {
+    constructor(props) {
+        super(props);
+
         axios.get('https://social-network.samuraijs.com/api/1.0/users')
             .then(response => {
-                return props.setUsers(response.data.items);
+                return this.props.setUsers(response.data.items);
             });
-    };
-    return (
-        <div className={c.usersContainer}>
-            <button onClick={getApiUsers}> Get Api Users</button>
-            {props.usersReducer.usersData.map((i, k) => {
-                return (
-                    <div key={k} className={c.userItem}>
-                        <div className={c.userItemImage}>
-                            <div className={c.imageButton}>
-                                <img src={i.photos.small ? i.photos.small : img} alt="photo"/>
-                                <div>
-                                    <button className={c.button}
-                                            onClick={(e) => {
-                                                e.currentTarget.innerText === 'Follow'
-                                                    ? props.followFunc(false, i.id)
-                                                    : props.followFunc(true, i.id)
-                                            }}>{i.followed ? 'Follow' : "UnFollow"}
-                                    </button>
+    }
+
+    render() {
+        return (
+            <div className={c.usersContainer}>
+                {this.props.usersReducer.usersData.map((i, k) => {
+                    return (
+                        <div key={k} className={c.userItem}>
+                            <div className={c.userItemImage}>
+                                <div className={c.imageButton}>
+                                    <img src={i.photos.small ? i.photos.small : img} alt="photo"/>
+                                    <div>
+                                        <button className={c.button}
+                                                onClick={(e) => {
+                                                    e.currentTarget.innerText === 'Follow'
+                                                        ? this.props.followFunc(false, i.id)
+                                                        : this.props.followFunc(true, i.id)
+                                                }}>{i.followed ? 'Follow' : "UnFollow"}
+                                        </button>
+                                    </div>
                                 </div>
                             </div>
+                            <div>User ID : {i.id}</div>
+                            <div>Name : {i.name}</div>
+                            <div>Status : {i.status}</div>
                         </div>
-                        <div>User ID : {i.id}</div>
-                        <div>Name : {i.name}</div>
-                        <div>Status : {i.status}</div>
-                    </div>
-                )
-            })}
-        </div>
-    )
-};
+                    )
+                })}
+            </div>
+        )
+    }
+}
+
 export default Users;
